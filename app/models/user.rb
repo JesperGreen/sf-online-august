eb6aa73d1006d21b7fea90c74ec7444c83e4b47a
Jesper <jesper_gs_@hotmail.com>
2016-09-29 11:31:29 +0200

    added validation of role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name, :address
+  validates_presence_of :name, :address, :role
 end
